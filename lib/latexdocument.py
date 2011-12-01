eab2457a75800d06d31635dfdccb74389928951b
Robert Schroll <rschroll@gmail.com>
2011-12-01 00:18:41 -0500

    Enable deleting of slides
    Closes #3.

--- lib/latexdocument.py
+++ lib/latexdocument.py
@@ -128,6 +128,9 @@ class LatexDocument(object):
         else:
             self.pages.append((slide, slide.pb))
     
+    def delete_page(self, iter):
+        self.pages.remove(iter)
+    
     def _save(self, fobj):
         fobj.write(self.header.get_content())
         for p in self.pages:
@@ -216,6 +219,7 @@ class LatexDocument(object):
                 </menu>
                 <menu action="slide">
                     <menuitem action="new-slide"/>
+                    <menuitem action="delete-slide"/>
                     <menuitem action="next-slide"/>
                     <menuitem action="prev-slide"/>
                 </menu>
@@ -234,6 +238,7 @@ class LatexDocument(object):
             <toolbar name="SlideBar" action="toolbar">
                 <separator/>
                 <toolitem action="new-slide"/>
+                <toolitem action="delete-slide"/>
                 <toolitem action="prev-slide"/>
                 <toolitem action="next-slide"/>
                 <separator/>
@@ -244,7 +249,7 @@ class LatexDocument(object):
         action_group.add_actions([
                 ('file',    None,       "_File"),
                 ('toolbar', None,       "Huh?"),
-                ('new',     gtk.STOCK_NEW,  "_New Presentation", "<control><shift>n", None, self.on_new_pres),
+                ('new',     gtk.STOCK_NEW,  "_New Presentation", "<control>n", None, self.on_new_pres),
                 ('open',    gtk.STOCK_OPEN, "_Open Presentation", "<control>o",   None, self.on_open_pres),
                 ('save',    gtk.STOCK_SAVE, "_Save",        "<control>s",   None, self.on_save),
                 ('save-as',  gtk.STOCK_SAVE_AS, "Save _As",  "<control><shift>s", None, self.save_as),
@@ -255,7 +260,8 @@ class LatexDocument(object):
                 ('compile-all',     gtk.STOCK_EXECUTE, "Compile Document", "<control><shift>Return", "Compile Document", self.on_compile_all),
                 
                 ('slide',  None,   "_Slide"),
-                ('new-slide', gtk.STOCK_NEW,  "_New Slide",   "<control>n",   None, self.on_new_slide),
+                ('new-slide', gtk.STOCK_NEW,          "_New Slide",       "<shift>Insert", None, self.on_new_slide),
+                ('delete-slide', gtk.STOCK_DELETE,    "_Delete Slide",    "<shift>Delete", None, self.on_delete_slide),
                 ('next-slide',  gtk.STOCK_GO_FORWARD, "Next Slide",       "Page_Down",     None, self.on_next_slide),
                 ('prev-slide',  gtk.STOCK_GO_BACK,    "Previous Slide",   "Page_Up",       None, self.on_prev_slide),
         ])
@@ -334,7 +340,9 @@ class LatexDocument(object):
     
     def on_selection_changed(self, view):
         selection = view.get_selected_items()
-        if len(selection) == 0 and self.prev_selection:
+        if len(selection) == 0 and self.prev_selection and len(self.pages):
+            if self.prev_selection[0] >= len(self.pages):
+                self.prev_selection = (len(self.pages) - 1,)
             view.select_path(self.prev_selection)
         elif len(selection) == 1:
             # Changing the buffer removes any selection, so we copy the
@@ -351,6 +359,8 @@ class LatexDocument(object):
                 self.viewer.load_doc(currslide.doc)
         else:
             self.currslide_view.set_sensitive(False)
+            if len(self.pages) == 0 and self.view_slide_button.get_active():
+                self.viewer.load_doc(None)
     
     # Right now, these two are used by drag-and-drop reordering
     def on_row_inserted(self, model, path, iter):
@@ -383,6 +393,11 @@ class LatexDocument(object):
         # Switch to slide editor and focus
         self.notebook.set_current_page(1)
         self.currslide_view.grab_focus()
+    
+    def on_delete_slide(self, action):
+        for selection in self.slidelist_view.get_selected_items():
+            iter = self.pages.get_iter(selection)
+            self.delete_page(iter)
     
     def on_compile_page(self, action):
         selection = self.slidelist_view.get_selected_items()
